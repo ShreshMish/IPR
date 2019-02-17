359259525d524e475c551e26b4cff294c07b42ad
Shyamal Patel <shyamal.patel@vivideconomics.com>
2019-02-17 16:23:42 +0000

    FINAL FIXUP: CC code and folder structure
    1. Consolidation of cost & competition code into 3 scripts
    a) Combine_datasets.R prepares the panel using DD and CM model results
    b) Model.R contains the functions to run the model
    c) Run_model.R reads in the panel (a), and runs the model using (b)
    Results from CC will be read in by asset impacts scripts to be written
    This is the final commit to this branch - merge into main as this is now a complete set of code.
    NB some bugs still need to be fixed (retained earlier to ensure comparability of Data cleaning and Model results)

--- Model_data_update.R
+++ Model_data_update.R
@@ -1,135 +1,138 @@
-##### Project code:       171211HSB - HSBC Low Carbon Portfolio
-##### Date of last edit:  22/01/2019
+##### Project code:       Net-Zero Toolkit for modelling the financial impacts of low-carbon transition scenarios
+##### Date of last edit:  17/02/2019
 ##### Code author:        Shyamal Patel
-##### Description:        This script refreshes the HSBC model data with new outputs from the Data cleaning folder
+##### Description:        This script refreshes the Net-zero Toolkit model data with new outputs from the Data cleaning folder
 ##### Dependencies:       Outputs from Data cleaning - ensure all file paths are still up-to-date
 
 #--------------------------------------------------------------------------------------------------
 
 ##### SECTION 1 - Housekeeping, data read in and useful functions for saving/writing files, and QA ----
 
-packages <- c("tidyverse", "magrittr", "readxl", "here", "stringi")
-lapply(packages, require, character.only = TRUE)
+source("utils.R")
 
-# Define date for save file names
-day <- format(Sys.time(), "%d")
-month <- match(format(Sys.time(), "%b"), month.abb)
-if(nchar(month) == 1) {month <- paste0("0", month)}
-year <- substr(format(Sys.time(), "%Y"), 3, 4)
-date <- paste0(year, month, day)
+# Define function for copying files based on source and destination paths
+copy_dated <- function(source_folder, source_file, dest_folder, dest_file) {
+  
+  source_file_path <- fs::path(here::here(data_cleaning), source_folder, paste0(source_file, ".rds"))
+  dest_file_path <- fs::path(here::here(models), dest_folder, paste0(dest_file, ".rds"))
+    
+  source_checksum_path <- fs::path(here::here(data_cleaning), source_folder, "Checksum", paste0(source_file, ".rds.txt"))
+  dest_checksum_path <- fs::path(here::here(models), dest_folder, "Checksum", paste0(dest_file, ".rds.txt"))
+  
+  # Only overwrite existing input data if new data cleaning output file is different to the old (digest / checksum approach)
+  checksum_source <- readChar(source_checksum_path, file.info(source_checksum_path)$size)
+  checksum_dest <- if(file.exists(dest_checksum_path)) {readChar(dest_checksum_path, file.info(dest_checksum_path)$size)} else {NA_character_}
 
-# These functions count the number of missing or zero-value observations in a tibble
-na_counter <- function(x) {sum(is.na(x))}
-zero_counter <- function(x) {sum(ifelse(x == 0, 1, 0))}
+  if(is.na(checksum_dest) | checksum_source != checksum_dest) {
+    file.copy(from = source_file_path, to = dest_file_path, overwrite = TRUE)
+    writeChar(checksum_source, dest_checksum_path, nchars = nchar(checksum_source, type = "chars"))
+    
+    # Date format for dated files folder
+    day <- format(Sys.time(), "%d")
+    month <-format(Sys.time(), "%m")
+    year <- substr(format(Sys.time(), "%Y"), 3, 4)
+    hour <- substr(format(Sys.time(), "%H"), 1, 2)
+    minute <- substr(format(Sys.time(), "%M"), 1, 2)
+    date_time_prefix <- paste0(year, month, day, "_", hour, minute, "_")
+
+    dest_dated_file_path <- fs::path(here::here(models), dest_folder, "Dated", paste0(date_time_prefix, dest_file, ".rds"))
+    file.copy(from = source_file_path, to = dest_dated_file_path, overwrite = TRUE)
+  }
+}
 
 # Assign location of data cleaning folder
-data_cleaning <- "Data cleaning/"
+data_cleaning <- "Data_cleaning"
 
 # Assign location of models folder
-models <- "Models/"
-
-# Define function for copying files based on source and destination paths, dated option
-copy_dated <- function(source_file, dest_folder, dest_file, dated = TRUE) {
-  
-  source_file_path <- paste0(data_cleaning, source_file, ".rds")
-  dest_file_path <- paste0(models, dest_folder, "/", dest_file, ".rds")
-  dated_dest_file_path <- paste0(models, dest_folder, "/Dated/", date, "_", dest_file, ".rds")
-  dated_dest_csvfile_path <- paste0(models, dest_folder, "/Dated/", date, "_", dest_file, ".csv")
-
-  file.copy(from = source_file_path, to = dest_file_path, overwrite = TRUE)
-  
-  # Save dated file (optional)
-  if(dated) {file.copy(from = source_file_path, to = dated_dest_file_path, overwrite = TRUE)}
-  
-}
+models <- "Models"
 
 #--------------------------------------------------------------------------------------------------
 
 ##### SECTION 2 - Update fossil fuel demand destruction data files ----
 
 # Define destination folder for fossil fuel demand destruction files
-ff_dest_folder <- "1 - Demand destruction/Input"
+dd_dest_folder <- "1_Demand_destruction/Input"
 
-# Results from alpha-factor adjustment (price_f (firm) = alpha * price_g (global))
-copy_dated(source_file = "4 - Fossil fuels/Oil and gas/Output/DD_alpha_results_production_adjusted",
-           dest_folder = ff_dest_folder,
-           dest_file = "DD_alpha_results_production_adjusted")
+# Oil and gas company-level data
+copy_dated(source_folder = "3_ESG/3b_Oil_and_gas/Output", source_file = "Model_prod_and_economic_data",
+           dest_folder = dd_dest_folder, dest_file = "Oil_and_gas_company_data")
 
-# DD compact dataset for oil and gas supermajor calculations
-copy_dated(source_file = "4 - Fossil fuels/Oil and gas/Output/DD_model_data_compact",
-           dest_folder = ff_dest_folder,
-           dest_file = "DD_model_data_compact")
+# Oil and gas company names data
+copy_dated(source_folder = "3_ESG/3b_Oil_and_gas/Output", source_file = "Companies_list",
+           dest_folder = dd_dest_folder, dest_file = "Oil_and_gas_company_names")
 
 # Oil and gas scenario data
-copy_dated(source_file = "0 - Scenarios/Output/Fossil_fuel_production",
-           dest_folder = ff_dest_folder,
-           dest_file = "Fossil_fuel_production")
+copy_dated(source_folder = "1_Scenarios/Output", source_file = "Fossil_fuel_production",
+           dest_folder = dd_dest_folder, dest_file = "Oil_and_gas_scenario_data")
 
 # Coal company-level data
-copy_dated(source_file = "4 - Fossil fuels/Coal/Output/Company_coal_data_cleaned",
-           dest_folder = ff_dest_folder,
-           dest_file = "Coal_company_data_cleaned")
+copy_dated(source_folder = "3_ESG/3c_Coal/Output", source_file = "Model_reg_exposure_data",
+           dest_folder = dd_dest_folder, dest_file = "Coal_company_data")
+
+# Coal company names data
+copy_dated(source_folder = "3_ESG/3c_Coal/Output", source_file = "Companies_list",
+           dest_folder = dd_dest_folder, dest_file = "Coal_company_names")
 
 # Coal cost curve data
-copy_dated(source_file = "4 - Fossil fuels/Coal/Output/Coal_cost_curve_full",
-           dest_folder = ff_dest_folder,
-           dest_file = "Coal_cost_curve_full")
+copy_dated(source_folder = "3_ESG/3c_Coal/Output", source_file = "Seaborne_supply_curve",
+           dest_folder = dd_dest_folder, dest_file = "Coal_cost_curve_data")
 
 # Coal scenario data (rename file to avoid confusion with oil and gas files)
-copy_dated(source_file = "4 - Fossil fuels/Coal/Output/Scenario_quantities",
-           dest_folder = ff_dest_folder,
-           dest_file = "Coal_scenario_quantities")
+copy_dated(source_folder = "1_Scenarios/Output", source_file = "Fossil_fuel_production",
+           dest_folder = dd_dest_folder, dest_file = "Coal_scenario_data")
 
 # ICE vehicle scenario data
-copy_dated(source_file = "0 - Scenarios/Output/ICE_new_capacity",
-           dest_folder = ff_dest_folder,
-           dest_file = "ICE_new_capacity")
+copy_dated(source_folder = "1_SCenarios/Output", source_file = "ICE_new_capacity",
+           dest_folder = dd_dest_folder, dest_file = "ICE_scenario_data")
 
 #--------------------------------------------------------------------------------------------------
 
-##### SECTION 3 - Update green upside data files ----
+##### SECTION 3 - Update cleantech markets data files ----
 
 # Define destination folder for green upside files
-gu_dest_folder <- "2 - Cleantech markets/Input"
+cm_dest_folder <- "2_Cleantech_markets/Input"
 
-# Results from green upside data cleaning
-copy_dated(source_file = "5 - Green upside/Output/Combined_Green_Revenue_data",
-           dest_folder = gu_dest_folder,
-           dest_file = "Combined_Green_Revenue_data")
+# Results from cleantech markets company data cleaning
+copy_dated(source_folder = "3_ESG/3d_Cleantech/Output", source_file = "Cleantech_patent_and_rev_data",
+           dest_folder = cm_dest_folder, dest_file = "Cleantech_company_data")
 
-# Renewable capacity dataset
-copy_dated(source_file = "0 - Scenarios/Output/Renewable_capacity",
-           dest_folder = gu_dest_folder,
-           dest_file = "Renewable_capacity")
+# Results from financial data cleaning (not the full panel)
+### TRY TO DELETE THIS DEPENDENCY IF POSSIBLE
+copy_dated(source_folder = "2_Financial/2a_Preliminary/Output", source_file = "Companies_2016USD_data",
+           dest_folder = cm_dest_folder, dest_file = "Financial_company_data")
 
-# EV dataset
-copy_dated(source_file = "0 - Scenarios/Output/EV_new_capacity",
-           dest_folder = gu_dest_folder,
-           dest_file = "EV_new_capacity")
+# Results from scenarios analysis for renewable capacity
+copy_dated(source_folder = "1_Scenarios/Output", source_file = "Renewable_capacity",
+           dest_folder = cm_dest_folder, dest_file = "Renewables_scenario_data")
+
+# Results from scenarios analysis for EV capacity
+copy_dated(source_folder = "1_Scenarios/Output", source_file = "EV_new_capacity",
+           dest_folder = cm_dest_folder, dest_file = "EVs_scenario_data")
 
 # Biofuels dataset
-copy_dated(source_file = "0 - Scenarios/Output/Biofuels_production",
-           dest_folder = gu_dest_folder,
-           dest_file = "Biofuels_production")
-
-# Cleaned financial dataset
-copy_dated(source_file = "1 - Financial prelim/Output/TR_cleaned_2016USD_data",
-           dest_folder = gu_dest_folder,
-           dest_file = "TR_cleaned_2016USD_data")
+copy_dated(source_folder = "1_Scenarios/Output", source_file = "Biofuels_production",
+           dest_folder = cm_dest_folder, dest_file = "Biofuels_scenario_data")
 
 #--------------------------------------------------------------------------------------------------
 
 ##### SECTION 4 - Update carbon cost data files ----
 
 # Define destination folder for carbon cost files
-cc_dest_folder <- "3 - Cost and competition/Input"
+cc_dest_folder <- "3_Cost_and_competition/Input"
 
 # Cleaned financial and emissions panel dataset
-copy_dated(source_file = "11 - Panel setting/Output/Model_panel",
-           dest_folder = cc_dest_folder,
-           dest_file = "Model_panel")
+copy_dated(source_folder = "4_Panel/Output", source_file = "Model_panel_final",
+           dest_folder = cc_dest_folder, dest_file = "Model_panel")
 
+# Cleaned carbon cost curves dataset
+copy_dated(source_folder = "4_Panel/Output", source_file = "Carbon_cost_curves",
+           dest_folder = cc_dest_folder, dest_file = "Carbon_cost_curves")
+
+# Results from scenarios analysis for carbon prices
+copy_dated(source_folder = "1_Scenarios/Output", source_file = "Model_co2_prices_2016USD",
+           dest_folder = cc_dest_folder, dest_file = "Carbon_prices_scenario_data")
+
+### TRY TO REMOVE THIS DEPENDENCY IF POSSIBLE
 # Green upside reclassification dataset
-copy_dated(source_file = "9 - Fossil and green upside prod/Output/Green_upside_reclassification",
-           dest_folder = cc_dest_folder,
-           dest_file = "Green_upside_reclassification")
+copy_dated(source_folder = "4_Panel/Output", source_file = "Green_upside_reclassification",
+           dest_folder = cc_dest_folder, dest_file = "Green_upside_reclassification")
