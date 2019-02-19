1bfd304445df0fdecc36b428a57f3cc8097475e8
Shyamal Patel <shyamal.patel@vivideconomics.com>
2019-02-19 11:35:13 +0000

    Scenario update: fossil fuel production
    1. Update fossil fuel production data cleaning code for new scenarios

--- Data_cleaning/1_Scenarios/Fossil_fuel_production.R
+++ Data_cleaning/1_Scenarios/Fossil_fuel_production.R
@@ -1,13 +1,9 @@
 ##### Project code:       Net-Zero Toolkit for modelling the financial impacts of low-carbon transition scenarios
-##### Date of last edit:  24/01/2019
+##### Date of last edit:  19/02/2019
 ##### Code author:        Shyamal Patel
 ##### Description:        This script reads in TIAM fossil fuel production data from Excel and cleans it for use in later calculations and modelling
 ##### Dependencies:       1.  Latest Imperial TIAM scenarios Excel file
 #####                     2.  IEA WEO scenarios data
-#####                     (current scenario data = "Input/Vivid_scenario_runs.xls" & "Input/Vivid_scenario_runs_newdb.xls" (for Weak 2020))
-
-##### TIAM has been updated with new technology costs and constraint changes, which is causing discrepancies between the same 
-##### scenario run in different versions - adjust 'weak 2020' scenario from new database to compensate for this
 
 #--------------------------------------------------------------------------------------------------
 
@@ -18,36 +14,76 @@ main_save_folder <- "1_Scenarios"
 source("utils.R")
 
 # Read in TIAM scenario data from old scenarios (skip 6 empty rows at the top of the sheet) - units are PJ
-tiam_fossil_fuel_prod <- read_excel(input_source("Vivid_scenario_runs.xls"),
+tiam_fossil_fuel_prod <- read_excel(input_source("Vivid_20190208.xlsx"),
                                     sheet = "AA_PrimaryEnergyProd", skip = 6)
 
-# Read in TIAM scenario data for weak 2020 scenario (skip 6 empty rows at the top of the sheet) - units are PJ
-tiam_w2020_fossil_fuel_prod <- read_excel(input_source("Vivid_scenario_runs_newdb.xls"),
-                                          sheet = "AA_PrimaryEnergyProd", skip = 6)
-
 # Read in IEA WEO scenario data - fossil fuel production (NB - must save a xlsx version of the macro-enabled WEO Annex A workbook) - units are in 'Units' column
-weo_raw_fossil_fuel_prod <- read_excel(input_source("WEO2017_AnnexA_VE.xlsx"),
-                                       sheet = "VE Fossil fuel prod", range = "$A$7:$H$10")
+public_scen_fossil_fuel_prod <- read_excel(input_source("Public_scenarios.xlsx"),
+                                           sheet = "W20. Fossil fuel prod", range = "$B$48:$Q$90")
 
 # Read in energy unit conversion factors
 energy_conversion_factors <- read_excel(input_source("Energy_conversion_factors.xlsx"),
                                         sheet = "Energy unit conversions", range = "$A$3:$G$8")
 
-# Read in scenario names (defined by Carbon_prices.R script)
-scenario_names <- readRDS("1_Scenarios/Interim/Scenario_name_mapping.rds")
+# Read in TIAM scenario names
+scenario_names <- read_excel(input_source("Vivid_scenario_names.xlsx"),
+                             sheet = "R1. Scenario specifications", range = "$A$10:$E$59")
 
-# Rename TIAM fuels to match Vivid descriptions
-fuel_names <- tibble(ProcessSet = unique(tiam_fossil_fuel_prod$ProcessSet)) %>%
-  mutate(fuel = case_when(ProcessSet == "MIN - COA" ~ "coal",
-                          ProcessSet == "MIN - GAS" ~ "gas",
-                          ProcessSet == "MIN - OIL" ~ "oil"),
-         units = "PJ")
+# Adjust factor for ratio of BP historical oil production data to TIAM data
+### IMPROVE AUDIT TRAIL HERE - CALCULATIONS IN R
+tiam_oil_adj_factor <- 1.1293
+tiam_coal_adj_factor <- 0.8147
+tiam_gas_adj_factor <- 0.9915
 
 #--------------------------------------------------------------------------------------------------
 
-##### SECTION 2 - Clean, reshape and change units of fossil fuel production data (2020 - 50) ----
+##### SECTION 2 - Clean and reshape public fossil fuel production scenario data (2017 - 50) ----
 
-## Turn energy conversion factors into named vector and add new factors
+# Clean up scenario names and remove unneeded scenarios (ETP17, WEO16, Shell Sky etc.)
+scenario_names2 <- scenario_names %>%
+  rename(old_scenario_name = `Scenario name`,
+         scenario = `Report name`,
+         public_scenario = `Public scen Report name`) %>%
+  select(old_scenario_name, scenario, public_scenario)
+
+public_scen_fossil_fuel_prod2 <- public_scen_fossil_fuel_prod %>%
+  rename(old_scenario_name = `Scenario`,
+         fuel = Fuel,
+         subcat = Subcat,
+         units = Units) %>%
+  gather(key = "year", value = "production", `2014`:`2050`) %>%
+  mutate(year = as.numeric(year)) %>%
+  left_join(scenario_names2, by = c("old_scenario_name")) %>%
+  select(public_scenario, fuel, subcat, units, year, production) %>%
+  rename(scenario = public_scenario)
+
+# Filter out WEO18 scenario data used for 'No_New_Action' and 'Paris_NDCs' scenarios
+weo18_fossil_fuel_prod <- public_scen_fossil_fuel_prod2 %>%
+  filter(!is.na(scenario)) %>%
+  # Rystad oil & gas data work requires 2016 values instead of 2017
+  filter(year >= 2016 & year != 2017)
+
+# Summarise over coal subcategories (thermal and coking)
+weo18_fossil_fuel_prod2 <- weo18_fossil_fuel_prod %>%
+  group_by(scenario, fuel, year, units) %>%
+  summarise(production = sum(production)) %>%
+  ungroup()
+
+# Switch oil units from Mb/d to Mb
+weo18_fossil_fuel_prod3 <- weo18_fossil_fuel_prod2 %>%
+  mutate(fuel = tolower(fuel)) %>%
+  mutate(production = case_when(fuel == "oil" ~ production * 365,
+                                TRUE ~ production),
+         units = case_when(fuel == "oil" ~ "Mb", 
+                           TRUE ~ units))
+
+save_dated(weo18_fossil_fuel_prod3, "WEO_fossil_production", folder = "Interim", csv = TRUE)
+
+#--------------------------------------------------------------------------------------------------
+
+##### SECTION 3 - Clean, reshape and change units of TIAM fossil fuel production data (2020 - 50) ----
+
+# Turn energy conversion factors into named vector and add new factors
 energy_conversion_factors_data <- as.vector(energy_conversion_factors$Value)
 names(energy_conversion_factors_data) <- energy_conversion_factors$Code
 
@@ -60,42 +96,45 @@ energy_conversion_factors_data["PJ_to_bcm"] <- energy_conversion_factors_data["P
 # Add new factor required for TIAM coal data - PJ to tce
 energy_conversion_factors_data["PJ_to_tce"] <- energy_conversion_factors_data["PJ_to_Mtoe"] / energy_conversion_factors_data["tce_to_toe"]
 
-## Gather, clean and rename TIAM scenario data
-fossil_fuel_prod <- tiam_fossil_fuel_prod %>%
-  gather(key = "year", value = "production", `2005`:`2100`) %>%
-  mutate(year = as.numeric(year)) %>%
+# Clean up dataframe names
+tiam_fossil_fuel_prod2 <- tiam_fossil_fuel_prod %>%
   rename(scenario = Scenario,
+         product = ProcessSet,
          region = `Region\\Period`) %>%
-  # Add units column and rename fuels
-  left_join(fuel_names, by = "ProcessSet") %>%
-  select(scenario, fuel, units, region, year, production)
-  
-# Remove surplus scenarios
-fossil_fuel_prod2 <- fossil_fuel_prod %>%
-  filter(!scenario %in% c("s06a_BHP_cum3dt_Highfeasibility", "s06a_BHP_cum4dt_Highfeasibility", "s06a_BHP_cumb2c300_Highfeasibility"))
-
-# Repeat for TIAm weak 2020 scenario data
-tiam_w2020_fossil_fuel_prod2 <- tiam_w2020_fossil_fuel_prod %>%
   gather(key = "year", value = "production", `2005`:`2100`) %>%
-  mutate(year = as.numeric(year)) %>%
-  rename(scenario = Scenario,
-         region = `Region\\Period`) %>%
-  # Add units column and rename fuels
-  left_join(fuel_names, by = "ProcessSet") %>%
-  select(scenario, fuel, units, region, year, production)
+  mutate(year = as.numeric(year))
 
-# For TIAM weak, keep "P2" scenarios (2020 start), and keep 2dt high feasbility and base for comparability
-tiam_w2020_fossil_fuel_prod3 <- tiam_w2020_fossil_fuel_prod2 %>%
-  filter(!scenario %in% c("s01a_BHP_base", "s01b_BHP_weak2020", "s06a_BHP_cum2dt_Highfeasibility")) %>%
-  # Remove 2025 year (blank values)
-  filter(year != 2025)
+# Merge in report scenario names and bin unrequired scenarios
+tiam_fossil_fuel_prod3 <- tiam_fossil_fuel_prod2 %>%
+  rename(old_scenario_name = scenario) %>%
+  left_join(scenario_names2, by = "old_scenario_name") %>%
+  select(-old_scenario_name, -public_scenario) %>%
+  filter(!is.na(scenario))
 
-# Join together TIAM datasets
-fossil_fuel_prod3 <- fossil_fuel_prod2 %>%
-  bind_rows(tiam_w2020_fossil_fuel_prod3)
+# Filter out primary energy production categories that are not used here
+tiam_fossil_fuel_prod4 <- tiam_fossil_fuel_prod3 %>%
+  filter(year <= 2050 & year >= 2020) %>%
+  mutate(fuel = case_when(product == "MIN - COA" ~ "coal",
+                          product == "MIN - GAS" ~ "gas",
+                          product == "MIN - OIL" ~ "oil",
+                          TRUE ~ NA_character_)) %>%
+  select(-product) %>%
+  filter(!is.na(fuel)) %>%
+  mutate(units = "PJ") %>%
+  select(scenario, region, fuel, units, year, production)
 
-# Change units to match IEA WEO datasets (starting units are PJ for all fuels)
-fossil_fuel_prod4 <- fossil_fuel_prod3 %>% 
+# Summarise fossil fuel categories over regions
+tiam_fossil_fuel_prod5 <- tiam_fossil_fuel_prod4 %>%
+  group_by(scenario, fuel, units, year) %>%
+  summarise(production = sum(production, na.rm = TRUE)) %>%
+  ungroup()
+
+# Remove 'No_New_action' and 'Paris_NDCs' scenarios from TIAM [using WEO18 for these]
+tiam_fossil_fuel_prod6 <- tiam_fossil_fuel_prod5 %>%
+  filter(!(scenario %in% c("No_New_Action", "Paris_NDCs")))
+
+# Switch TIAM scenario units [oil -> Mb, coal -> Mtce, gas -> bcm]
+tiam_fossil_fuel_prod7 <- tiam_fossil_fuel_prod6 %>%
   mutate(production = case_when(fuel == "coal" ~ production * energy_conversion_factors_data["PJ_to_tce"],
                                 fuel == "oil" ~ production * energy_conversion_factors_data["PJ_to_Mb"],
                                 fuel == "gas" ~ production * energy_conversion_factors_data["PJ_to_bcm"]),
@@ -103,111 +142,56 @@ fossil_fuel_prod4 <- fossil_fuel_prod3 %>%
                            fuel == "oil" ~ "Mb", # Million barrels of oil per annum
                            fuel == "gas" ~ "bcm")) # Billion cubic metres of natural gas per annum
 
-# Sum over regions
-fossil_fuel_prod5 <- fossil_fuel_prod4 %>%
-  group_by(scenario, fuel, units, year) %>%
-  summarise(production = sum(production, na.rm = TRUE)) %>% 
-  ungroup() %>%
-  # Filter extra years
-  filter(year <= 2050)
+# Adjust TIAM values by ratio of historical BP Stat review to TIAM
+tiam_fossil_fuel_prod8 <- tiam_fossil_fuel_prod7 %>%
+  mutate(production = case_when(fuel == "coal" ~ production * tiam_coal_adj_factor,
+                                fuel == "oil" ~ production * tiam_oil_adj_factor,
+                                fuel == "gas" ~ production * tiam_gas_adj_factor))
 
-save_dated(fossil_fuel_prod5, "fossil_fuel_production", folder = "Interim", csv = TRUE)
-
-# Rename scenarios based on model names
-fossil_fuel_prod6 <- fossil_fuel_prod5 %>%
-  left_join(scenario_names) %>%
-  select(model_scenario, year, fuel, units, production) %>%
-  rename(scenario = model_scenario)
+# Merge together TIAM scenario data and WEO18 scenario data
+fossil_fuel_prod <- tiam_fossil_fuel_prod8 %>%
+  bind_rows(weo18_fossil_fuel_prod3)
 
 #--------------------------------------------------------------------------------------------------
 
-##### SECTION 3 - Ad hoc changes to carbon price data - please check when updating scenario inputs ----
+##### SECTION 4 - Ad hoc changes to fossil fuel production data - please check when updating scenario inputs ----
 
 # Put scenarios in columns before further data cleaning
-fossil_fuel_prod7 <- fossil_fuel_prod6 %>%
+fossil_fuel_prod2 <- fossil_fuel_prod %>%
   spread(key = scenario, value = production)
 
-# Adjust Paris_INDCs scenario by ratio of old and new database scenarios (2DS_central and Base)
-# Value in 2020 should be equal to other scenarios (which follow the oldDB Weak 2020 to 2020)
-fossil_fuel_prod8 <- fossil_fuel_prod7 %>%
-  mutate(ratio_2DS_central = (`2DS_central` / `2DS_central_newDB`),
-         ratio_base = (BAU / BAU_newDB),
-         ratio_adjustment = (ratio_base + ratio_2DS_central) / 2,
-         Paris_INDCs = case_when(year == 2020 ~ `2DS_central`,
-                                 year != 2020 ~ Paris_INDCs * ratio_adjustment)) %>%
-  # Drop ratios and new DB scenarios other than weak 2020
-  select(-ratio_base, -ratio_2DS_central, - ratio_adjustment, -BAU_newDB, -`2DS_central_newDB`)
+# Fill in 2016 and 2020 WEO Paris values in place of TIAM values, 2030 WEO Paris values in place of TIAM Late_Action scenario values
+fossil_fuel_prod3 <- fossil_fuel_prod2 %>%
+  group_by(fuel, units) %>%
+  mutate_at(vars(unique(tiam_fossil_fuel_prod7$scenario)),
+            funs(case_when(year == 2016 ~ Paris_NDCs,
+                           year == 2020 ~ Paris_NDCs,
+                           TRUE ~ .))) %>%
+  mutate(Late_Action = ifelse(year == 2030, Paris_NDCs, Late_Action)) %>%
+  ungroup()
 
-# Clean IEA scenario data and merge in
-weo_fossil_fuel_prod <- weo_raw_fossil_fuel_prod %>%
-  rename(scenario = Scenario,
-         fuel = Product,
-         units = Units) %>%
-  mutate(scenario = "WEO_CPS") %>%
-  # Maintain calculations for 2050 (straight line through 2030, 2040, 2050)
-  mutate(`2050` = `2040` + (`2040` - `2030`)) %>%
-  # Change fuel names to lower case
-  mutate(fuel = str_to_lower(fuel))
+# Interpolate missing years (2025, 2035, 2045)
+fossil_fuel_prod4 <- fossil_fuel_prod3 %>%
+  group_by(fuel, units) %>%
+  # Also apply to WEO scenarios for consistency
+  mutate_at(vars(unique(weo18_fossil_fuel_prod3$scenario)),
+            funs(ifelse(year %in% c(2025, 2035, 2045), NA_real_, .))) %>%
+  mutate_at(vars(c(unique(weo18_fossil_fuel_prod3$scenario), unique(tiam_fossil_fuel_prod7$scenario))),
+            funs(approx(x = year, y = ., xout = year)$y)) %>%
+  ungroup()
 
-# Reshape IEA scenario data
-weo_fossil_fuel_prod2 <- weo_fossil_fuel_prod %>%
-  gather(key = "year", value = "production", `2015`:`2050`) %>%
-  spread(key = "scenario", value = "production") %>%
-  # Change units of oil (million barrels per day, to million barrels)
-  mutate(year = as.numeric(year),
-         WEO_CPS = ifelse(fuel == "oil", WEO_CPS * 365, WEO_CPS),
-         units = case_when(fuel == "coal" ~ "Mtce",
-                           fuel == "oil" ~ "Mb",
-                           fuel == "gas" ~ "bcm"))
-
-# Merge IEA WEO CPS into TIAM scenario data
-fossil_fuel_prod9 <- fossil_fuel_prod8 %>%
-  full_join(weo_fossil_fuel_prod2) %>%
-  arrange(fuel, year)
-
-# Apply changes made in old scenarios spreadsheet
-fossil_fuel_prod10 <- fossil_fuel_prod9 %>%
-  filter(year >= 2016) %>%
-  # Interpolate IEA scenario data for 2020
-  group_by(fuel) %>%
-  mutate(WEO_CPS = approx(x = year, y = WEO_CPS, xout = year)$y) %>%
-  ungroup() %>%
-  # Replace all TIAM scenario values with IEA WEO CPS for years up to 2020
-  mutate_at(.vars = vars(`2DS_central`:`Paris_INDCs`),
-            .funs = funs(ifelse(year <= 2020, WEO_CPS, .))) %>%
-  # Replace 2DS_delay scenario values with IEA WEO CPS for years up to 2030
-  mutate(`2DS_delay` = ifelse(year > 2020 & year <= 2030, WEO_CPS, `2DS_delay`)) %>%
-  # Replace BAU scenario values with IEA WEO CPS for all years
-  mutate(BAU = ifelse(year > 2020, WEO_CPS, BAU))
-
-# Expand grid to cover 2035, 2045
-fossil_fuel_prod11 <- expand.grid(year = c(2016, seq(2020, 2050, by = 5)),
-                                fuel = unique(fossil_fuel_prod10$fuel)) %>%
-  left_join(fossil_fuel_prod10) %>%
-  # Fill in units
-  group_by(fuel) %>%
-  fill(units) %>%
-  # Interpolate scenario quantities to fill missing years
-  mutate_at(.vars = vars(`2DS_central`:`Paris_INDCs`),
-            .funs = funs(approx(x = year, y = ., xout = year)$y)) %>%
-  ungroup() %>%
-  # Cap INDCs scenario at BAU values
-  mutate(Paris_INDCs = ifelse(Paris_INDCs > BAU, BAU, Paris_INDCs))
-
-# Drop WEO_CPS and reshape
-fossil_fuel_prod12 <- fossil_fuel_prod11 %>%
-  select(-WEO_CPS) %>%
-  gather(key = "scenario", value = "production", `2DS_central`:`Paris_INDCs`) %>%
-  select(scenario, fuel, units, year, production) %>%
-  arrange(scenario, fuel, year)
-         
-# Change units to match spreadsheet outputs
-fossil_fuel_prod13 <- fossil_fuel_prod12 %>%
-  mutate(production = case_when(fuel == "gas" ~ production * 10^9,
-                                fuel == "oil" ~ production * 10^6,
-                                fuel == "coal" ~ production * 10^6)) %>%
-  mutate(units = case_when(fuel == "gas" ~ "cm",
+# Reshape and change units to match old spreadsheet outputs (preserved to save on editions to the other code)
+fossil_fuel_prod5 <- fossil_fuel_prod4 %>%
+  gather(key = "scenario", value = "production", -(fuel:year)) %>%
+  mutate(production = case_when(fuel == "gas" ~ production * 10^9, # Gas units: bcm -> cm
+                                fuel == "oil" ~ production * 10^6, # Oil units: Mbarrels -> barrels
+                                fuel == "coal" ~ production * 10^6), # Coal units: Mtonnes -> tonnes
+         units = case_when(fuel == "gas" ~ "cm",
                            fuel == "oil" ~ "barrels",
                            fuel == "coal" ~ "tonnes"))
 
-save_dated(fossil_fuel_prod13, "Fossil_fuel_production", folder = "Output", csv = TRUE)
+# Reorder variables for clarity
+fossil_fuel_prod6 <- fossil_fuel_prod5 %>%
+  select(scenario, fuel, units, year, production)
+
+save_dated(fossil_fuel_prod6, "Fossil_fuel_production", folder = "Output", csv = TRUE)
